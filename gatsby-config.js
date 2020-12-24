b24225003a9be7dc4700293647570a999f1440bf
Subha Chandra <subha_chandra@apple.com>
2020-12-24 20:36:51 +0530

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ module.exports = {
     title: 'Subha Chandra',
     description:
       'Subha Chandra is a software engineer specializing in building (and occasionally designing) exceptional websites, applications, and everything in between.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://jsubhachandra.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@jsubhachandra',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'SubhaChandra',
+        short_name: 'SubhaChandra',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
